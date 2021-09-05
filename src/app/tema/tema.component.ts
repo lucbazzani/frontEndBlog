7b1ae00d0523ca0179994b28d51c2fe1408ef89b
lucbazzani <lu.bazzani@hotmail.com>
2021-09-05 11:25:17 -0300

    implementação de get e post de temas

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -23,14 +23,22 @@ export class TemaComponent implements OnInit {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
   }
 
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
       this.tema = resp
       alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
       this.tema = new Tema()
     })
   }
-
 }
